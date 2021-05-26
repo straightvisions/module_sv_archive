d5f64d3f3996cd9ec9fd43fceb5d2383cd5f340d
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-05-26 18:19:48 +0200

    UPDATE TEMPLATE

--- sv_archive.php
+++ sv_archive.php
@@ -29,6 +29,22 @@
 				->get_root()
 				->add_section( $this );
 
+			// remove prefixes from archive titles
+			add_filter( 'get_the_archive_title', function ($title) {
+				if ( is_category() ) {
+					$title = single_cat_title( '', false );
+				} elseif ( is_tag() ) {
+					$title = single_tag_title( '', false );
+				} elseif ( is_author() ) {
+					$title = '<span class="vcard">' . get_the_author() . '</span>' ;
+				} elseif ( is_tax() ) { //for custom post types
+					$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+				} elseif (is_post_type_archive()) {
+					$title = post_type_archive_title( '', false );
+				}
+				return $title;
+			});
+
 			$this->load_settings();
 		}
 		public function go(): sv_archive{
